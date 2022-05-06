10bd96bc77504262458ab94c94b49f4e5c7205ed
andreamisu <misu.it.official@gmail.com>
2022-05-06 18:57:15 +0200

    eheuhehu did miss the continue

--- sketch.js
+++ sketch.js
@@ -88,11 +88,11 @@ function draw() {
     if (capture.pixels) {     
       for (y = 0; y < capture.height; y+=params.pixelSize) {
          for (x = 0; x < windowWidth; x+=params.pixelSize) {
-
           if(x < widthPresence / 2 || x > capture.width + widthPresence / 2){
             //black
             const letter = characters[Math.round(map(255, 0, 255, characters.length - 1, 0))]
             text(" ", x, y )
+            continue
           }
           // *4 is for each rgba value 
           const index = ((x - (widthPresence/2)) + y * capture.width) * 4
