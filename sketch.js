9a22425ca0bc2c706c1a5b4e6952c5dec2518106
andreamisu <misu.it.official@gmail.com>
2022-05-12 00:03:47 +0200

    appropriate scale + css shadows

--- sketch.js
+++ sketch.js
@@ -17,6 +17,8 @@ let capture
 let capturing = false
 let canvasWidth
 let canvasHeight
+let ratioCapture
+let flagRatio = true
 
 function getRightCaputreSize(width){
   return width * aspectRatio
@@ -48,9 +50,14 @@ function setup() {
 }
 
 function draw() {
+  if(flagRatio){
+    ratioHeight = windowHeight / capture.height
+    ratioWidth = windowWidth / capture.width
+    ratioCapture = ratioHeight > ratioWidth ? ratioHeight : ratioWidth
+    flagRatio = false
+  }
   
-  ratioHeight = windowHeight / capture.height
-  scale(ratioHeight)
+  scale(ratioCapture)
   push()
   
   background(0)
@@ -66,36 +73,11 @@ function draw() {
   if (capturing) {
     capture.loadPixels()
 
-
-    // let actualWidth = capture.height + (windowWidth-capture.width)
-    // if (capture.pixels) {     
-    //     for (y = 0; y < capture.height; y+=params.pixelSize) {
-    //        for (x = 0; x < capture.width; x+=params.pixelSize) {
-    //       // *4 is for each rgba value
-    //       const index = (x + y * capture.width) * 4
-    //       const r = capture.pixels[index] 
-    //       const g = capture.pixels[index + 1] 
-    //       const b = capture.pixels[index + 2] 
-    //       const a = capture.pixels[index + 3] 
-    //       const bright = Math.round((r + g + b) / 3)                  
-    //       const letter = characters[Math.round(map(bright, 0, 255, characters.length - 1, 0))]
-    //       text(letter, x, y )
-    //     }
-    //   }
-    // }
-
-    let widthPresence = windowWidth - capture.width
     if (capture.pixels) {     
-      for (y = 0; y < capture.height; y+=params.pixelSize) {
-         for (x = 0; x < windowWidth; x+=params.pixelSize) {
-          if(x < widthPresence / (2 * ratioHeight) || x > capture.width + widthPresence / (2 *ratioHeight)){
-            //black
-            const letter = characters[Math.round(map(255, 0, 255, characters.length - 1, 0))]
-            text(" ", x, y )
-            continue
-          }
-          // *4 is for each rgba value 
-          const index = ((x - (widthPresence/2)) + y * capture.width) * 4
+        for (y = 0; y < capture.height; y+=params.pixelSize) {
+           for (x = 0; x < capture.width; x+=params.pixelSize) {
+          // *4 is for each rgba value
+          const index = (x + y * capture.width) * 4
           const r = capture.pixels[index] 
           const g = capture.pixels[index + 1] 
           const b = capture.pixels[index + 2] 
@@ -106,6 +88,29 @@ function draw() {
         }
       }
     }
+
+    // let widthPresence = windowWidth - capture.width
+    // if (capture.pixels) {     
+    //   for (y = 0; y < capture.height; y+=params.pixelSize) {
+    //      for (x = 0; x < windowWidth; x+=params.pixelSize) {
+    //       if(x < widthPresence / (2 * ratioHeight) || x > capture.width + widthPresence / (2 *ratioHeight)){
+    //         //black
+    //         const letter = characters[Math.round(map(255, 0, 255, characters.length - 1, 0))]
+    //         text(" ", x, y )
+    //         continue
+    //       }
+    //       // *4 is for each rgba value 
+    //       const index = ((x - (widthPresence/2)) + y * capture.width) * 4
+    //       const r = capture.pixels[index] 
+    //       const g = capture.pixels[index + 1] 
+    //       const b = capture.pixels[index + 2] 
+    //       const a = capture.pixels[index + 3] 
+    //       const bright = Math.round((r + g + b) / 3)                  
+    //       const letter = characters[Math.round(map(bright, 0, 255, characters.length - 1, 0))]
+    //       text(letter, x, y )
+    //     }
+    //   }
+    // }
   } 
   
 }
